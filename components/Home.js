c943c3364cb32b28e838b13a80f089ef6eed270d
Prince Chauhan <prince.chauhan.cs.2019@miet.ac.in>
2022-05-24 05:12:10 +0000

    master

--- components/Home.js
+++ components/Home.js
@@ -398,14 +398,6 @@ class Transactionslist extends Component {
         );
     }
 }
-function ProfileScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Profile screen</Text>
-            <Button title="Go back" onPress={() => navigation.goBack()} />
-        </View>
-    );
-}
 
 const Stack = createNativeStackNavigator();
 
