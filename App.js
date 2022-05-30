b2c9d5714d3b4d30824ac206e396aa6ad7d823ba
Prince Chauhan <prince.chauhan.cs.2019@miet.ac.in>
2022-05-30 20:12:17 +0530

    adding redux to store session

--- App.js
+++ App.js
@@ -2,6 +2,12 @@
 import React from 'react';
 import LoginApp from './components/LoginPage';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const store = createStore()
+
 export default function App() {
   return (
     <LoginApp></LoginApp>
